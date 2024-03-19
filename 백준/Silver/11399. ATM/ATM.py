c79ef06ad1d63f5f0e385df70e2cfc3405770ff8
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-03-19 23:20:51 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,7 +1,8 @@
 import sys
 
 n = int(sys.stdin.readline())
-numbers = sorted(list(map(int, sys.stdin.readline().split())))
+numbers = list(map(int, sys.stdin.readline().split()))
+numbers.sort()
 sum = 0
 for i in range(n):
     sum += numbers[i] * (n - i)
