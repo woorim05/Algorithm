a1e4957fa2f24394ed8ef29738181091ecc50828
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-03-20 23:38:17 +0900

    [Silver V] Title: 숫자 카드, Time: 668 ms, Memory: 125176 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,13 +1,10 @@
 import sys
 
 n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
+a = set(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 b = list(map(int, sys.stdin.readline().split()))
-arr = [0] * (20000000 + 1)
-
-for i in a:
-    arr[i + 10000000] = 1
 
 for i in b:
-    print(arr[i + 10000000], end=" ")
+    if i in a: print(1, end=" ")
+    else: print(0, end=" ")
