3790d9074b62615f619ccbbf8491fc139c1e1ecf
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-03-21 22:32:25 +0900

    [Silver IV] Title: 듣보잡, Time: 100 ms, Memory: 43924 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,10 +1,12 @@
-n, m = map(int, input().split())
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
 a = set()
 b = set()
 for _ in range(n):
-    a.add(input())
+    a.add(sys.stdin.readline().strip())
 for _ in range(m):
-    b.add(input())
+    b.add(sys.stdin.readline().strip())
 result = sorted(a & b)
 print(len(result))
 print(*result, sep="\n")
