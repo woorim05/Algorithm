63d49d140499192a049be52a8ab7d4d45fb228f7
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-04-07 21:20:21 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 924 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,11 @@
+def hanoi(n, one, two, three):
+    if n == 1:
+        print(one, three)
+        return 
+    hanoi(n - 1, one, three, two)
+    print(one, three)
+    hanoi(n - 1, two, one, three)
+
+n=int(input())
+print((2 ** n) - 1)
+hanoi(n, 1, 2, 3)
