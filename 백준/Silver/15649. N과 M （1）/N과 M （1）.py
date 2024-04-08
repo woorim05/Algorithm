d445a02660ddf4e587d9fdc78fae1ee5e65a1a2b
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-04-08 20:06:15 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,15 @@
+def dfs():
+    if len(numbers) == m:
+        print(*numbers)
+        return
+    
+    for i in range(1, n + 1):
+        if i not in numbers:
+            numbers.append(i)
+            dfs()
+            numbers.pop()
+
+n, m = map(int, input().split())
+numbers = []
+result = []
+dfs()
