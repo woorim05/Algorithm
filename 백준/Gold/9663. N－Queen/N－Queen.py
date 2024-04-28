9ddc4ed5c038dae0e5784c6c129021ddd4058e5b
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-04-28 15:34:35 +0900

    [Gold IV] Title: N-Queen, Time: 23996 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+pos = [0] * n
+flag_a = [False] * n
+flag_b = [False] * (2*n - 1)
+flag_c = [False] * (2*n - 1)
+
+cnt = 0
+
+def set(i):
+    global cnt
+    for j in range(n):
+        if not flag_a[j] and not flag_b[i + j] and not flag_c[i - j + n-1]:
+            pos[i] = j
+            if i == n-1:
+                cnt += 1
+            else:
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + n-1] = True
+                set(i + 1)
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + n-1] = False
+set(0)
+print(cnt)
