aca340447491442223205e2c1a9af815d3c9de25
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-06-01 22:04:51 +0900

    [Gold IV] Title: 오큰수, Time: 1132 ms, Memory: 155548 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -4,24 +4,10 @@ input  = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().rstrip().split()))
 stack = []
-nges = [-1 for _ in range(N)]
-for i in range(N-1, -1, -1):
-    number = A[i]
-    
-    if not stack:
-        nges[i] = -1
-        stack.append(number)
-        continue
-    
-    top = stack[-1]
-    if number < top:
-        nges[i] = top
-    else:
-        while stack and stack[-1] <= number:
-            stack.pop()
+nges = [-1] * N
 
-        if stack: nges[i] = stack[-1]
-    
-    stack.append(number)
-    
+for i in range(N):
+    while stack and A[stack[-1]] < A[i]:
+        nges[stack.pop()] = A[i]
+    stack.append(i)
 print(*nges)
